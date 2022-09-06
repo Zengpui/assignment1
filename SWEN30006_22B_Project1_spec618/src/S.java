c9e4157974974bf4b0265e8dc3fb64909b2f4a9e
Zengpui <zhouzhengpei1234@gmail.com>
2022-09-07 06:19:55 +1000

    New changes

---
+++ SWEN30006_22B_Project1_spec618/src/S.java
@@ -0,0 +1,53 @@
+// S.java
+package src;
+import ch.aplu.jgamegrid.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class S extends Piece
+{
+
+
+
+  S(Tetris tetris) {
+
+    super(tetris, 4, "S");
+
+    Location[][] r = new Location[4][4];
+
+    // rotId 0
+    r[0][0] = new Location(new Location(1, 0));
+    r[1][0] = new Location(new Location(0, 0));
+    r[2][0] = new Location(new Location(0, 1));
+    r[3][0] = new Location(new Location(-1, 1));
+// rotId 1
+    r[0][1] = new Location(new Location(0, 1));
+    r[1][1] = new Location(new Location(0, 0));
+    r[2][1] = new Location(new Location(-1, 0));
+    r[3][1] = new Location(new Location(-1, -1));
+// rotId 2
+    r[0][2] = new Location(new Location(-1, 0));
+    r[1][2] = new Location(new Location(0, 0));
+    r[2][2] = new Location(new Location(0, -1));
+    r[3][2] = new Location(new Location(1, -1));
+// rotId 3
+    r[0][3] = new Location(new Location(0, -1));
+    r[1][3] = new Location(new Location(0, 0));
+    r[2][3] = new Location(new Location(1, 0));
+    r[3][3] = new Location(new Location(1, 1));
+
+
+
+
+    this.setR(r);
+
+    for (int i = 0; i < r.length; i++)
+      blocks.add(new TetroBlock(this.getBlockId(), r[i]));
+  }
+
+
+
+
+
+}
